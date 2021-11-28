3551fc8b01272ad6b18ea778bbdaece8fca56827
Eric Hess <eric.hess.13@gmail.com>
2021-11-28 10:43:12 -0600

    Added some more data to vb_pbp return

--- ncaa_vb_pbp.R
+++ ncaa_vb_pbp.R
@@ -1,4 +1,6 @@
-
+#TODO: can we use starter info, and sub info to determine who was on the court for every point?
+#TODO: collapse plays into single scoring events (column for scoring text, home subs, away subs)
+#TODO: ignore warnings on the score string split
 ncaa_vb_pbp <- function(game_id) {
   base_url <- "https://data.ncaa.com/casablanca/game/"
   
@@ -20,7 +22,14 @@ ncaa_vb_pbp <- function(game_id) {
   pbp <- pbp.df %>%
     purrr::map_if(is.data.frame, list) %>%
     dplyr::as_tibble() %>%
-    tidyr::unnest(.data$playStats)
+    tidyr::unnest(.data$playStats) %>%
+    tidyr::separate(.data$score, c("visitorScore", "homeScore")) %>%
+    dplyr::mutate(playType = ifelse(!is.na(.data$homeScore), "Scoring", "Non-Scoring"),
+                  matchPlayNumber = row_number()) %>%
+    dplyr::group_by(periodNumber) %>%
+    dplyr::mutate(setPlayNumber = row_number())
+  
+  pbp[,3:4] <- sapply(pbp[,3:4],as.numeric)
   
   return(pbp)
 }
