adb7547461115da8fe91ba486af660b64b6faeda
Eric Hess <eric.hess.13@gmail.com>
2021-12-04 17:09:09 -0600

    Turn code into package. Added FB documentation

--- ncaa_fb_box_score.R
+++ ncaa_fb_box_score.R
@@ -1,57 +1,66 @@
-
+#' ncaa_fb_box_score
+#'
+#' Obtains football game team box scores from the NCAA API.
+#'
+#' @param game_id the game_id of a given NCAA FB game from the NCAA API
+#'
+#' @return a data.frame with NCAA FB player box score data for the given game
+#'
+#' @example ncaa_fb_team_box_score(5931773)
+#' @export
 ncaa_fb_box_score <- function(game_id) {
   base_url <- "https://data.ncaa.com/casablanca/game/"
-  
+
   full_url <- paste(base_url, game_id, "boxscore.json", sep="/")
-  
+
   # Check for internet
   #check_internet()
-  
+
   # Create the GET request and set response as res
   res <- httr::GET(full_url)
-  
+
   # Check the result
   #check_status(res)
-  
+
   bs.json <- jsonlite::fromJSON(full_url, flatten = TRUE)
-  
+
   bs.df <- as.data.frame(bs.json$tables)
-  
+
   bs.df2 <- bs.df %>%
     purrr::map_if(is.data.frame, list) %>%
     dplyr::as_tibble() %>%
     tidyr::unnest(.data$data, names_repair = "unique")
-  
+
   data <- bs.df2 %>%
     purrr::map_if(is.data.frame, list) %>%
     dplyr::as_tibble() %>%
-    tidyr::unnest(.data$row, names_repair = "unique") 
-  
+    tidyr::unnest(.data$row, names_repair = "unique")
+
   headers <- bs.df2 %>%
     purrr::map_if(is.data.frame, list) %>%
     dplyr::as_tibble() %>%
     tidyr::unnest(.data$header, names_repair = "unique")
-  
+
   #Rename data
   combined <- cbind(data, headers)
   colnames(combined) <- c("id", "headerColor", "headerClass", "header", "playerCategory", "display", "total",
                           "id2", "headerColor2", "headerClass2", "statCategory", "headerdisplay", "row", "total2")
-  
+
   #Assign player name to each row
-  formatted <- combined %>% select(id, headerColor, headerClass, playerCategory, display, total, headerdisplay) %>% 
+  formatted <- combined %>% select(id, headerColor, headerClass, playerCategory, display, total, headerdisplay) %>%
     mutate(playerid = cumsum(case_when(playerCategory == "playerName" ~ 1,
-                                       TRUE ~ 0))) %>% 
-    group_by(playerid) %>% 
-    mutate(playerName = trimws(first(display))) %>% 
-    ungroup() %>% 
+                                       TRUE ~ 0))) %>%
+    group_by(playerid) %>%
+    mutate(playerName = trimws(first(display))) %>%
+    ungroup() %>%
     dplyr::select(-playerid) %>%
     dplyr::filter(is.na(playerCategory))
-  
+
   #Clarify duplicate column names
   formatted$headerdisplay <- apply(formatted, 1, function(x) {
     id <- as.character(x["id"])
     stat.name <- as.character(x["headerdisplay"])
-    
+
     if (id == "rushing_visiting" | id == "rushing_home") {
       return(paste0("Rushing.", stat.name))
     } else if (id == "receiving_visiting" | id == "receiving_home") {
@@ -63,24 +72,24 @@ ncaa_fb_box_score <- function(game_id) {
     } else if (id == "kick_returns_visiting" | id == "kick_returns_home") {
       return(paste0("Kick.Return.", stat.name))
     }
-    
+
     return(stat.name)
   })
-  
+
   #Remove total fields, only focus on players
-  no.total <- formatted %>% tidyr::pivot_wider(names_from = .data$headerdisplay, values_from = .data$display) %>% 
-    tidyr::separate(.data$`Passing.CP-ATT-INT`, c("PassingComp", "PassingAtt", "PassingInt"), "-") %>% 
-    #tidyr::separate(.data$`FG-FGA`, c("FG", "FGA"), "/") %>% 
-    dplyr::filter(is.na(total)) %>% 
+  no.total <- formatted %>% tidyr::pivot_wider(names_from = .data$headerdisplay, values_from = .data$display) %>%
+    tidyr::separate(.data$`Passing.CP-ATT-INT`, c("PassingComp", "PassingAtt", "PassingInt"), "-") %>%
+    #tidyr::separate(.data$`FG-FGA`, c("FG", "FGA"), "/") %>%
+    dplyr::filter(is.na(total)) %>%
     dplyr::select(-c(headerClass, playerCategory, total, id, headerColor))
-  
+
   no.total[,2:34] <- sapply(no.total[,2:34],as.numeric)
   no.total[is.na(no.total)] <- 0
-  
-  final <- no.total %>% 
-    group_by(playerName) %>% 
-    summarise(across(everything(), sum)) %>% 
+
+  final <- no.total %>%
+    group_by(playerName) %>%
+    summarise(across(everything(), sum)) %>%
     janitor::clean_names()
-  
+
   return(final)
 }
