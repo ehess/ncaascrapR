33eebdbf215a15ee9592ecec7a5d4181c854e437
john-b-edwards <edwards1860@gmail.com>
2021-12-19 23:58:16 -0500

    Update bb_box_score.R
    some edge case fixes, re-ordering names to be first_name last_name instead of last_name, first_name, including team stats (such as team rebounds) as a player

--- bb_box_score.R
+++ bb_box_score.R
@@ -4,105 +4,134 @@
 #'
 #' @param game_id the game_id for a given game on stats.ncaa.org
 #' @return a dataframe for box score data for that game
-get_bb_player_box <- function(game_id){
-  html <- paste0('https://stats.ncaa.org/game/box_score/',game_id) |>
+get_bb_player_box <- function(game_id) {
+  html <- paste0('https://stats.ncaa.org/game/box_score/', game_id) |>
     rvest::read_html()
   html |>
     rvest::html_node(xpath = '/html/body/div[2]/table[5]') |>
     rvest::html_table() |>
-    dplyr::filter(X1 != X2 & !(X1 %in% c('Player','TEAM','Totals'))) |>
-    dplyr::mutate(player = X1,
-                  player_id = html |>
-                    rvest::html_node(xpath = '/html/body/div[2]/table[5]') |>
-                    rvest::html_nodes('a') |>
-                    rvest::html_attr('href') |>
-                    (\(x) gsub('_seq=','', stringi::stri_extract_all_regex(x,'_seq=([0-9]*)$')))(),
-                  team = html |>
-                    rvest::html_node(xpath = '/html/body/div[2]/table[5]') |>
-                    rvest::html_node('.heading') |>
-                    rvest::html_text() |>
-                    trimws(),
-                  team_id = html |>
-                    rvest::html_nodes(xpath = '/html/body/div[2]/table[1]/tr/td/a') |>
-                    rvest::html_attr('href') |>
-                    (\(x) gsub('/teams/','',x))() |>
-                    dplyr::nth(1),
-                  pos = X2,
-                  g = as.integer(X3),
-                  min = lubridate::ms(X4),
-                  fgm = as.integer(X5),
-                  fga = as.integer(X6),
-                  fgpct = fgm / fga,
-                  tpm = as.integer(X7),
-                  tpa = as.integer(X8),
-                  tppct = tpm / tpa,
-                  ftm = as.integer(X9),
-                  fta = as.integer(X10),
-                  ftpct = ftm / fta,
-                  pts = as.integer(X11),
-                  oreb = as.integer(X12),
-                  dreb = as.integer(X13),
-                  treb = as.integer(X14),
-                  ast = as.integer(X15),
-                  to = as.integer(X16),
-                  stl = as.integer(X17),
-                  blk = as.integer(X18),
-                  personal_fouls = as.integer(X19),
-                  dq = as.integer(X20),
-                  technical_fouls = as.integer(X21),
-                  dplyr::across(where(is.integer), ~ dplyr::case_when(is.na(.x) ~ as.integer(0),
-                                                                    T~.x)),
-                  dplyr::across(dplyr::ends_with('pct'), ~ dplyr::case_when(is.na(.x) ~ NaN,
-                                                                            T~.x))) |>
+    janitor::row_to_names(2) |>
+    dplyr::filter(Player != 'Totals') |>
+    dplyr::mutate(
+      player = gsub(',','',sub("(^.*),\\s(.*$)","\\2 \\1", Player)),
+      player_id = html |>
+        rvest::html_node(xpath = '/html/body/div[2]/table[5]') |>
+        rvest::html_nodes('a') |>
+        rvest::html_attr('href') |>
+        (\(x) gsub(
+          '_seq=',
+          '',
+          stringi::stri_extract_all_regex(x, '_seq=([0-9]*)$')
+        ))() |>
+        (\(x) c(x, ''))(),
+      team = html |>
+        rvest::html_node(xpath = '/html/body/div[2]/table[5]') |>
+        rvest::html_node('.heading') |>
+        rvest::html_text() |>
+        trimws(),
+      team_id = html |>
+        rvest::html_nodes(xpath = '/html/body/div[2]/table[1]/tr/td/a') |>
+        rvest::html_attr('href') |>
+        (\(x) gsub('/teams/', '', x))() |>
+        dplyr::nth(1),
+      pos = Pos,
+      g = as.integer(G),
+      min = lubridate::ms(MP, roll = T, quiet = T),
+      fgm = as.integer(FGM),
+      fga = as.integer(FGA),
+      fgpct = dplyr::case_when(is.na(fga) ~ NaN, is.na(fgm) ~ 0, T ~ fgm / fga),
+      tpm = as.integer(`3FG`),
+      tpa = as.integer(`3FGA`),
+      tppct = dplyr::case_when(is.na(tpa) ~ NaN, is.na(tpm) ~ 0, T ~ tpm / tpa),
+      ftm = as.integer(FT),
+      fta = as.integer(FTA),
+      ftpct = dplyr::case_when(is.na(fta) ~ NaN, is.na(ftm) ~ 0, T ~ ftm / fta),
+      pts = as.integer(PTS),
+      oreb = as.integer(ORebs),
+      dreb = as.integer(DRebs),
+      treb = as.integer(`Tot Reb`),
+      ast = as.integer(AST),
+      to = as.integer(TO),
+      stl = as.integer(STL),
+      blk = as.integer(BLK),
+      personal_fouls = as.integer(gsub('/', '', Fouls)),
+      dq = as.integer(gsub('/', '', DQ)),
+      technical_fouls = as.integer(gsub('/', '', `Tech Fouls`)),
+      dplyr::across(
+        where(is.integer),
+        ~ dplyr::case_when(is.na(.x) ~ as.integer(0),
+                           T ~
+                             .x)
+      )
+    ) |>
     dplyr::select(player:technical_fouls) |>
-    dplyr::bind_rows(html |>
-                       rvest::html_node(xpath = '/html/body/div[2]/table[6]') |>
-                       rvest::html_table() |>
-                       dplyr::filter(X1 != X2 & !(X1 %in% c('Player','TEAM','Totals'))) |>
-                       dplyr::mutate(player = X1,
-                                     player_id = html |>
-                                       rvest::html_node(xpath = '/html/body/div[2]/table[6]') |>
-                                       rvest::html_nodes('a') |>
-                                       rvest::html_attr('href') |>
-                                       (\(x) gsub('_seq=','', stringi::stri_extract_all_regex(x,'_seq=([0-9]*)$')))(),
-                                     team = html |>
-                                       rvest::html_node(xpath = '/html/body/div[2]/table[6]') |>
-                                       rvest::html_node('.heading') |>
-                                       rvest::html_text() |>
-                                       trimws(),
-                                     team_id = html |>
-                                       rvest::html_nodes(xpath = '/html/body/div[2]/table[1]/tr/td/a') |>
-                                       rvest::html_attr('href') |>
-                                       (\(x) gsub('/teams/','',x))() |>
-                                       dplyr::nth(2),
-                                     pos = X2,
-                                     g = as.integer(X3),
-                                     min = lubridate::ms(X4),
-                                     fgm = as.integer(X5),
-                                     fga = as.integer(X6),
-                                     fgpct = fgm / fga,
-                                     tpm = as.integer(X7),
-                                     tpa = as.integer(X8),
-                                     tppct = tpm / tpa,
-                                     ftm = as.integer(X9),
-                                     fta = as.integer(X10),
-                                     ftpct = ftm / fta,
-                                     pts = as.integer(X11),
-                                     oreb = as.integer(X12),
-                                     dreb = as.integer(X13),
-                                     treb = as.integer(X14),
-                                     ast = as.integer(X15),
-                                     to = as.integer(X16),
-                                     stl = as.integer(X17),
-                                     blk = as.integer(X18),
-                                     personal_fouls = as.integer(X19),
-                                     dq = as.integer(X20),
-                                     technical_fouls = as.integer(X21),
-                                     dplyr::across(where(is.integer), ~ dplyr::case_when(is.na(.x) ~ as.integer(0),
-                                                                                         T~.x)),
-                                     dplyr::across(dplyr::ends_with('pct'), ~ dplyr::case_when(is.na(.x) ~ NaN,
-                                                                                               T~.x))) |>
-                       dplyr::select(player:technical_fouls))
+    dplyr::bind_rows(
+      html |>
+        rvest::html_node(xpath = '/html/body/div[2]/table[6]') |>
+        rvest::html_table() |>
+        janitor::row_to_names(2) |>
+        dplyr::filter(Player != 'Totals') |>
+        dplyr::mutate(
+          player = gsub(',','',sub("(^.*),\\s(.*$)","\\2 \\1", Player)),
+          player_id = html |>
+            rvest::html_node(xpath = '/html/body/div[2]/table[6]') |>
+            rvest::html_nodes('a') |>
+            rvest::html_attr('href') |>
+            (\(x) gsub(
+              '_seq=',
+              '',
+              stringi::stri_extract_all_regex(x, '_seq=([0-9]*)$')
+            ))()  |>
+            (\(x) c(x, ''))(),
+          team = html |>
+            rvest::html_node(xpath = '/html/body/div[2]/table[6]') |>
+            rvest::html_node('.heading') |>
+            rvest::html_text() |>
+            trimws(),
+          team_id = html |>
+            rvest::html_nodes(xpath = '/html/body/div[2]/table[1]/tr/td/a') |>
+            rvest::html_attr('href') |>
+            (\(x) gsub('/teams/', '', x))() |>
+            dplyr::nth(2),
+          pos = Pos,
+          g = as.integer(G),
+          min = lubridate::ms(MP, roll = T, quiet =
+                                T),
+          fgm = as.integer(FGM),
+          fga = as.integer(FGA),
+          fgpct = dplyr::case_when(is.na(fga) ~ NaN, is.na(fgm) ~ 0, T ~ fgm / fga),
+          tpm = as.integer(`3FG`),
+          tpa = as.integer(`3FGA`),
+          tppct = dplyr::case_when(is.na(tpa) ~ NaN, is.na(tpm) ~ 0, T ~ tpm / tpa),
+          ftm = as.integer(FT),
+          fta = as.integer(FTA),
+          ftpct = dplyr::case_when(is.na(fta) ~ NaN, is.na(ftm) ~ 0, T ~ ftm / fta),
+          pts = as.integer(PTS),
+          oreb = as.integer(ORebs),
+          dreb = as.integer(DRebs),
+          treb = as.integer(`Tot Reb`),
+          ast = as.integer(AST),
+          to = as.integer(TO),
+          stl = as.integer(STL),
+          blk = as.integer(BLK),
+          personal_fouls = as.integer(gsub('/', '', Fouls)),
+          dq = as.integer(gsub('/', '', DQ)),
+          technical_fouls = as.integer(gsub('/', '', `Tech Fouls`)),
+          dplyr::across(
+            where(is.integer),
+            ~ dplyr::case_when(is.na(.x) ~ as.integer(0),
+                               T ~
+                                 .x)
+          ),
+          dplyr::across(
+            dplyr::ends_with('pct'),
+            ~ dplyr::case_when(is.na(.x) ~ NaN,
+                               T ~
+                                 .x)
+          )
+        ) |>
+        dplyr::select(player:technical_fouls)
+    )
 }
 
 #' get_bb_team_box
@@ -111,89 +140,112 @@ get_bb_player_box <- function(game_id){
 #'
 #' @param game_id the game_id for a given game on stats.ncaa.org
 #' @return a dataframe for box score data for that game
-get_bb_team_box <- function(game_id){
-  html <- paste0('https://stats.ncaa.org/game/box_score/',game_id) |>
+get_bb_team_box <- function(game_id) {
+  html <- paste0('https://stats.ncaa.org/game/box_score/', game_id) |>
     rvest::read_html()
   html |>
     rvest::html_node(xpath = '/html/body/div[2]/table[5]') |>
     rvest::html_table() |>
-    dplyr::filter(X1 == 'Totals') |>
-    dplyr::mutate(team = html |>
-                    rvest::html_node(xpath = '/html/body/div[2]/table[5]') |>
-                    rvest::html_node('.heading') |>
-                    rvest::html_text() |>
-                    trimws(),
-                  team_id = html |>
-                    rvest::html_nodes(xpath = '/html/body/div[2]/table[1]/tr/td/a') |>
-                    rvest::html_attr('href') |>
-                    (\(x) gsub('/teams/','',x))() |>
-                    dplyr::nth(1),
-                  g = as.integer(X3),
-                  min = lubridate::ms(X4),
-                  fgm = as.integer(X5),
-                  fga = as.integer(X6),
-                  fgpct = fgm / fga,
-                  tpm = as.integer(X7),
-                  tpa = as.integer(X8),
-                  tppct = tpm / tpa,
-                  ftm = as.integer(X9),
-                  fta = as.integer(X10),
-                  ftpct = ftm / fta,
-                  pts = as.integer(X11),
-                  oreb = as.integer(X12),
-                  dreb = as.integer(X13),
-                  treb = as.integer(X14),
-                  ast = as.integer(X15),
-                  to = as.integer(X16),
-                  stl = as.integer(X17),
-                  blk = as.integer(X18),
-                  personal_fouls = as.integer(X19),
-                  dq = as.integer(X20),
-                  technical_fouls = as.integer(X21),
-                  dplyr::across(where(is.integer), ~ dplyr::case_when(is.na(.x) ~ as.integer(0),
-                                                                      T~.x)),
-                  dplyr::across(dplyr::ends_with('pct'), ~ dplyr::case_when(is.na(.x) ~ NaN,
-                                                                            T~.x))) |>
+    janitor::row_to_names(2) |>
+    dplyr::filter(Player == 'Totals') |>
+    dplyr::mutate(
+      team = html |>
+        rvest::html_node(xpath = '/html/body/div[2]/table[5]') |>
+        rvest::html_node('.heading') |>
+        rvest::html_text() |>
+        trimws(),
+      team_id = html |>
+        rvest::html_nodes(xpath = '/html/body/div[2]/table[1]/tr/td/a') |>
+        rvest::html_attr('href') |>
+        (\(x) gsub('/teams/', '', x))() |>
+        dplyr::nth(1),
+      min = lubridate::ms(MP, roll = T, quiet = T),
+      fgm = as.integer(FGM),
+      fga = as.integer(FGA),
+      fgpct = dplyr::case_when(is.na(fga) ~ NaN, is.na(fgm) ~ 0, T ~ fgm / fga),
+      tpm = as.integer(`3FG`),
+      tpa = as.integer(`3FGA`),
+      tppct = dplyr::case_when(is.na(tpa) ~ NaN, is.na(tpm) ~ 0, T ~ tpm / tpa),
+      ftm = as.integer(FT),
+      fta = as.integer(FTA),
+      ftpct = dplyr::case_when(is.na(fta) ~ NaN, is.na(ftm) ~ 0, T ~ ftm / fta),
+      pts = as.integer(PTS),
+      oreb = as.integer(ORebs),
+      dreb = as.integer(DRebs),
+      treb = as.integer(`Tot Reb`),
+      ast = as.integer(AST),
+      to = as.integer(TO),
+      stl = as.integer(STL),
+      blk = as.integer(BLK),
+      personal_fouls = as.integer(gsub('/', '', Fouls)),
+      dq = as.integer(gsub('/', '', DQ)),
+      technical_fouls = as.integer(gsub('/', '', `Tech Fouls`)),
+      dplyr::across(
+        where(is.integer),
+        ~ dplyr::case_when(is.na(.x) ~ as.integer(0),
+                           T ~
+                             .x)
+      ),
+      dplyr::across(
+        dplyr::ends_with('pct'),
+        ~ dplyr::case_when(is.na(.x) ~ NaN,
+                           T ~
+                             .x)
+      )
+    ) |>
     dplyr::select(team:technical_fouls) |>
-    dplyr::bind_rows(html |>
-                       rvest::html_node(xpath = '/html/body/div[2]/table[6]') |>
-                       rvest::html_table() |>
-                       dplyr::filter(X1 == 'Totals') |>
-                       dplyr::mutate(team = html |>
-                                       rvest::html_node(xpath = '/html/body/div[2]/table[6]') |>
-                                       rvest::html_node('.heading') |>
-                                       rvest::html_text() |>
-                                       trimws(),
-                                     team_id = html |>
-                                       rvest::html_nodes(xpath = '/html/body/div[2]/table[1]/tr/td/a') |>
-                                       rvest::html_attr('href') |>
-                                       (\(x) gsub('/teams/','',x))() |>
-                                       dplyr::nth(2),
-                                     g = as.integer(X3),
-                                     min = lubridate::ms(X4),
-                                     fgm = as.integer(X5),
-                                     fga = as.integer(X6),
-                                     fgpct = fgm / fga,
-                                     tpm = as.integer(X7),
-                                     tpa = as.integer(X8),
-                                     tppct = tpm / tpa,
-                                     ftm = as.integer(X9),
-                                     fta = as.integer(X10),
-                                     ftpct = ftm / fta,
-                                     pts = as.integer(X11),
-                                     oreb = as.integer(X12),
-                                     dreb = as.integer(X13),
-                                     treb = as.integer(X14),
-                                     ast = as.integer(X15),
-                                     to = as.integer(X16),
-                                     stl = as.integer(X17),
-                                     blk = as.integer(X18),
-                                     personal_fouls = as.integer(X19),
-                                     dq = as.integer(X20),
-                                     technical_fouls = as.integer(X21),
-                                     dplyr::across(where(is.integer), ~ dplyr::case_when(is.na(.x) ~ as.integer(0),
-                                                                                         T~.x)),
-                                     dplyr::across(dplyr::ends_with('pct'), ~ dplyr::case_when(is.na(.x) ~ NaN,
-                                                                                               T~.x))) |>
-                       dplyr::select(team:technical_fouls))
+    dplyr::bind_rows(
+      html |>
+        rvest::html_node(xpath = '/html/body/div[2]/table[6]') |>
+        rvest::html_table() |>
+        janitor::row_to_names(2) |>
+        dplyr::filter(Player == 'Totals') |>
+        dplyr::mutate(
+          team = html |>
+            rvest::html_node(xpath = '/html/body/div[2]/table[5]') |>
+            rvest::html_node('.heading') |>
+            rvest::html_text() |>
+            trimws(),
+          team_id = html |>
+            rvest::html_nodes(xpath = '/html/body/div[2]/table[1]/tr/td/a') |>
+            rvest::html_attr('href') |>
+            (\(x) gsub('/teams/', '', x))() |>
+            dplyr::nth(1),
+          min = lubridate::ms(MP, roll = T, quiet =
+                                T),
+          fgm = as.integer(FGM),
+          fga = as.integer(FGA),
+          fgpct = dplyr::case_when(is.na(fga) ~ NaN, is.na(fgm) ~ 0, T ~ fgm / fga),
+          tpm = as.integer(`3FG`),
+          tpa = as.integer(`3FGA`),
+          tppct = dplyr::case_when(is.na(tpa) ~ NaN, is.na(tpm) ~ 0, T ~ tpm / tpa),
+          ftm = as.integer(FT),
+          fta = as.integer(FTA),
+          ftpct = dplyr::case_when(is.na(fta) ~ NaN, is.na(ftm) ~ 0, T ~ ftm / fta),
+          pts = as.integer(PTS),
+          oreb = as.integer(ORebs),
+          dreb = as.integer(DRebs),
+          treb = as.integer(`Tot Reb`),
+          ast = as.integer(AST),
+          to = as.integer(TO),
+          stl = as.integer(STL),
+          blk = as.integer(BLK),
+          personal_fouls = as.integer(gsub('/', '', Fouls)),
+          dq = as.integer(gsub('/', '', DQ)),
+          technical_fouls = as.integer(gsub('/', '', `Tech Fouls`)),
+          dplyr::across(
+            where(is.integer),
+            ~ dplyr::case_when(is.na(.x) ~ as.integer(0),
+                               T ~
+                                 .x)
+          ),
+          dplyr::across(
+            dplyr::ends_with('pct'),
+            ~ dplyr::case_when(is.na(.x) ~ NaN,
+                               T ~
+                                 .x)
+          )
+        ) |>
+        dplyr::select(team:technical_fouls)
+    )
 }
